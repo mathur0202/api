1ce30ea83fce287815f0775a4d916cc49eb2eaac
gaurav mathur <int-gaurav.mathur@1mg.com>
2022-03-23 12:12:24 +0530

    Add routes.py
    this comiit add routes

--- app/routes/corporate.py
+++ app/routes/corporate.py
@@ -15,6 +15,15 @@ async def get_all_corporate(request):
 @corporate.route("/corporate", methods=['POST'])
 @validator(schema=Schema.corporate_schema)
 async def create_corporate(request: Request):
+    """
+        this is a Post route which interacts with the database to create a new corporate entry.
+        We take the corporate details and pass it to CorporateManager where it will be stored in db.
+        See implementation at manager -> corporate_manager.py.
+
+        :params request: request object
+        :return: response object in json format
+
+    """
     payload = request.custom_json()
     _response = await CorporateManager.create_corporate(payload)
     return send_response(_response)
@@ -25,3 +34,19 @@ async def get_corporate_by_name(request: Request,name):
     _response = await CorporateManager.get_corporate_by_name(payload)
     _response['id'] = str(_response['id'])
     return send_response(_response)
+
+@corporate.route("/presignedurl",methods=['GET'])
+async def get_presigned_url(request: Request):
+    """
+        this is a Get Api endpoint which when hit will call a function
+        named generate_presigned_url See implementation at manager -> corporate_manager.py.
+        which will then return us a presigned url.
+    """
+
+    query_params = request.request_params()
+    query_params = {
+            "Key": query_params.get('filename'),
+            "ContentType": query_params.get('ContentType')
+        }
+    response = await CorporateManager.generate_presigned_url(**query_params)
+    return send_response(response)
